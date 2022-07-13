2fa02c726a0bf1ddf975cdd5a8fe2d2be9961801
fschiro <75554993+fschiro@users.noreply.github.com>
2022-07-13 17:28:11 -0400

    Update main.r

--- solver/main.r
+++ solver/main.r
@@ -8,7 +8,7 @@ library(magrittr)
 stem <- "/media/frank/T7/"
 root <- stem %>% paste0("dev/dRumschitzki Lab/r_solver_v2/")
 
-root %>% paste0("inc/map_grid_matrix_fun.r") %>% source() # get map grid -> matrix
+root %>% paste0("inc/map_grid_matrix_fun_v2.r") %>% source() # get map grid -> matrix
 # get shripad grid functions
 root %>% paste0("inc/gridCreation/rGridMultipleNew.r") %>% source() 
 root %>% paste0("inc/gridCreation/zggrid.r") %>% source() 
@@ -16,5 +16,7 @@ root %>% paste0("inc/gridCreation/zigrid_new.r") %>% source()
 root %>% paste0("inc/gridCreation/zmgrid_new.r") %>% source() 
 root %>% paste0("inc/gridCreation/zmgrid_new.r") %>% source() 
 root %>% paste0("inc/variables.r") %>% source() 
+root %>% paste0("inc/one_sided_FD.r") %>% source() 
+root %>% paste0("inc/two_sided_FD_v1.r") %>% source() 
 
 #root %>% paste0("createGrid.r") %>% source() 
