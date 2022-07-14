d1a98fe26a2250c5e8699123202122e0587990db
fschiro <75554993+fschiro@users.noreply.github.com>
2022-07-14 09:23:56 -0400

    Update map_grid_matrix_fun_v2.r

--- solver/inc/map_grid_matrix_fun_v2.r
+++ solver/inc/map_grid_matrix_fun_v2.r
@@ -47,6 +47,43 @@ gMapX_ipmx_jpmx <- function(i, j, gRows = 4, gCols = 5, add_i = 0, add_j = 0){
 # If you call negative index in matrix you get error so can't have values where i or j -1
 # if you call positive values no error for some reason so i+1 works
 # ======================================================== #
+
+# Need to test speed before moving v2 to production
+# Error on one computer:  Error in .local(x, i, j, ..., value) :  RHS 'value' (class list) matches 'ANY', but must match matrix class dsCMatrix 
+# Implement version control!!
+
+map_equations_to_matrix_v2 <- function(
+        A, rows_in_z, rows_in_r, ij = NULL, 
+        ip1j = NULL, ip2j = NULL, ip3j = NULL, im1j = NULL, im2j = NULL, im3j = NULL,
+        ijp1 = NULL, ijp2 = NULL, ijp3 = NULL, ijm1 = NULL, ijm2 = NULL, ijm3 = NULL, 
+        gridPoints = NULL
+    ){
+    # gridPoints should be output of expand.grid(seq( i_points), seq(j_points) )
+        # null gridpoints creates new
+    # A is matrix to update
+    # ij, ip1j, im1j, ijp1, ijm1 are vectors of length gridpoints_x * gridpoints_y to add to matrix
+    if(is.null(gridPoints)) gridPoints <- expand.grid(seq(rows_in_z), seq(rows_in_r))
+    gridPoints %<>% split(., seq(nrow(.)))
+    
+	df = data.frame(
+		key = c('ij', 'ip1j', 'ip2j', 'ip3j', 'im1j', 'im2j', 'im3j', 'ijp1', 'ijp2', 'ijp3', 'ijm1', 'ijm2', 'ijm3'),
+		add_i = c(0, 1, 2, 3, -1, -2, -3, 0, 0, 0, 0, 0, 0),
+		add_j = c(0, 0, 0, 0, 0, 0, 0, 1, 2, 3, -1, -2, -3)
+	)
+	matrices = list(ij, ip1j, ip2j, ip3j, im1j, im2j, im3j, ijp1, ijp2, ijp3, ijm1, ijm2, ijm3)
+		
+	for(i in seq(nrow(df))){
+		if(!is.null(matrices[i])){
+			index <- gridPoints %>% lapply(function(x) gMapX_ipmx_jpmx(x[[1]], x[[2]], rows_in_z, rows_in_r, add_i = df$add_i[i], add_j = df$add_j[i])) %>% do.call(rbind, .)
+			A[ index[index[, 1] > 0 & index[, 2] > 0, ] ] <- matrices[i][ index[index[, 1] > 0 & index[, 2] > 0, 1] ]
+		}
+	}	
+	
+	A %>% return()
+    
+}
+                                           
+                                           
 map_equations_to_matrix <- function(
         A, rows_in_z, rows_in_r, ij = NULL, 
         ip1j = NULL, im1j = NULL, ip2j = NULL, im2j = NULL,
