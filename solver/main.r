d0f034cb6b9720c2654e912163aafa789d74f919
fschiro <75554993+fschiro@users.noreply.github.com>
2022-07-22 11:01:04 -0400

    Update main.r

--- solver/main.r
+++ solver/main.r
@@ -3,20 +3,14 @@
 # ======================================================== #
 
 
-
 library(magrittr)
-stem <- "/media/frank/T7/"
-root <- stem %>% paste0("dev/dRumschitzki Lab/r_solver_v2/")
 
-root %>% paste0("inc/map_grid_matrix_fun_v2.r") %>% source() # get map grid -> matrix
+file.path('inc', 'map_grid_matrix_fun_v2.r') %>% source
 # get shripad grid functions
-root %>% paste0("inc/gridCreation/rGridMultipleNew.r") %>% source() 
-root %>% paste0("inc/gridCreation/zggrid.r") %>% source() 
-root %>% paste0("inc/gridCreation/zigrid_new.r") %>% source() 
-root %>% paste0("inc/gridCreation/zmgrid_new.r") %>% source() 
-root %>% paste0("inc/gridCreation/zmgrid_new.r") %>% source() 
-root %>% paste0("inc/variables.r") %>% source() 
-root %>% paste0("inc/one_sided_FD.r") %>% source() 
-root %>% paste0("inc/two_sided_FD_v1.r") %>% source() 
-
-#root %>% paste0("createGrid.r") %>% source() 
+file.path('inc', 'gridCreation', 'rGridMultipleNew.r') %>% source
+file.path('inc', 'gridCreation', 'zggrid.r') %>% source
+file.path('inc', 'gridCreation', 'zigrid_new.r') %>% source
+file.path('inc', 'gridCreation', 'zmgrid_new.r') %>% source
+file.path('inc', 'variables.r') %>% source
+file.path('inc', 'one_sided_FD.r') %>% source
+file.path('inc', 'two_sided_FD_v2.r') %>% source
