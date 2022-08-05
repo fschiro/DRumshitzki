5720e94992e531ee751cdd036a7084b47e764665
fschiro <75554993+fschiro@users.noreply.github.com>
2022-08-05 15:56:55 -0400

    Update one_sided_FD.r

--- solver/inc/one_sided_FD.r
+++ solver/inc/one_sided_FD.r
@@ -13,3 +13,16 @@ omega_3 <- -1 * (2 * dzd + dz2d) / (dz2d * (dzd + dz2d))
 omega_4 <- dzu / (dz2u * (dzu + dz2u))
 omega_5 <- -1 * (dzu + dz2u) / (dzu * dz2u)
 omega_6 <- (2 * dzu + dz2u) / (dz2u * (dzu + dz2u))
+
+
+
+# ================================================== #
+# Run some tests & quit program if tests fail 
+# ================================================== #
+
+finiteTest(omega_1, 'omega_1')
+finiteTest(omega_2, 'omega_2')
+finiteTest(omega_3, 'omega_3')
+finiteTest(omega_4, 'omega_4')
+finiteTest(omega_5, 'omega_5')
+finiteTest(omega_6, 'omega_6')
