83626628f31fca02f0fc06d891cb3026c52ca052
fschiro <75554993+fschiro@users.noreply.github.com>
2022-08-19 11:19:33 -0400

    Update depend.r

--- solver/inc/depend.r
+++ solver/inc/depend.r
@@ -1,5 +1,6 @@
 
 library(magrittr)
+library(measurements) # https://www.rdocumentation.org/packages/measurements/versions/1.4.0/topics/conv_unit # conv_unit(3, from='m', to='cm')
 
 # test if a vector has any NaN, Inf, or NA. If it does, throw an error and stop program
 finiteTest <- function(numeric_vector, label){
@@ -12,3 +13,5 @@ logger <- function(msg, file = 'log.txt', console = TRUE){
 	log_msg %>% cat(file = file, append = TRUE)
 	if(console) log_msg %>% cat()
 }
+
+
