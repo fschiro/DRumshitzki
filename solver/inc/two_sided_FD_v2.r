ccd2d4abaa36ff066ce49fd5d4eac8fcc0b305c5
fschiro <75554993+fschiro@users.noreply.github.com>
2022-08-19 11:17:00 -0400

    Update two_sided_FD_v2.r

--- solver/inc/two_sided_FD_v2.r
+++ solver/inc/two_sided_FD_v2.r
@@ -6,11 +6,19 @@
 # D2(P, r) * H_SQR
 # ===================== #
 # P(r - Hb, z) 2 / (Hb * (Hb + Hf)) * P(r - Hb, z)
-betaAlt_1 <- H_SQR * rep(2 / (dr_backward * (dr_forward + dr_backward)), rows_in_z)
+
+beta_1 <- rep(2 / (dr_backward * (dr_forward + dr_backward)), rows_in_z)
 # P(r, z) [- 2 / (Hb * Hf) ]
-betaAlt_2 <- H_SQR * rep(-2 / (dr_forward * dr_backward), rows_in_z)
+beta_2 <- rep(-2 / (dr_forward * dr_backward), rows_in_z)
 # P(r + Hf, z) 2 / (Hf * (Hb + Hf)) * P(r + Hf, z)
-betaAlt_3 <- H_SQR * rep(2 / (dr_forward * (dr_forward + dr_backward)), rows_in_z) 
+beta_3 <- rep(2 / (dr_forward * (dr_forward + dr_backward)), rows_in_z) 
+
+
+betaAlt_1 <- H_SQR * beta_1
+# P(r, z) [- 2 / (Hb * Hf) ]
+betaAlt_2 <- H_SQR * beta_2
+# P(r + Hf, z) 2 / (Hf * (Hb + Hf)) * P(r + Hf, z)
+betaAlt_3 <- H_SQR * beta_3
 
 # ========================== #
 # D2(P, z)
@@ -26,16 +34,19 @@ beta_6 <- 2 / (dzd * (dzu + dzd))
 # D(P, r) * [1 / r] * H_SQR
 # ========================== #
 # P(r - Hb, z)  Hf / (Hb * (Hb + Hf))
+gamma_1 <- rep( dr_forward / (dr_backward * (dr_forward + dr_backward) ), rows_in_z )
 gammaAlt_1 <- H_SQR * rep( dr_forward / (dr_backward * (dr_forward + dr_backward) ) * r_inverse, rows_in_z )
 # P(r, z)  [- (Hb - Hf) / (Hb * Hf) ] 
+gamma_2 <- rep( (-dr_backward + dr_forward) / (dr_backward * dr_forward), rows_in_z )
 gammaAlt_2 <- H_SQR * rep( (-dr_backward + dr_forward) / (dr_backward * dr_forward) * r_inverse, rows_in_z )
 # P(r + Hf, z)  Hb / (Hf * (Hb + Hf)) 
+gamma_3 <- rep( dr_backward / (dr_forward * (dr_forward + dr_backward) ), rows_in_z )
 gammaAlt_3 <- H_SQR * rep( dr_backward / (dr_forward * (dr_forward + dr_backward) ) * r_inverse, rows_in_z )
 
 # ========================== #
 # D(P, z) (for endothelial cells)
 # ========================== #
-# P(r, z + Hd) = Hd / [hu * (Hu + Hd)] + EC_TOP_ONLY [ - a_j ]
+# P(r, z - Hd) = Hd / [hu * (Hu + Hd)] + EC_TOP_ONLY [ - a_j ]
 gamma_4 <- dzd / ( dzu * (dzu + dzd) )
 # P(r, z) = (Hu - Hd) / (Hu * Hd)
 gamma_5 <- (dzu - dzd) / (dzu * dzd)
